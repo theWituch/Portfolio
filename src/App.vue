465293a2ec936d0fbbed160180f56057aad209c3
theWituch <thewituch@gmail.com>
2022-08-05 05:27:05 +0200

    Add basic styling and theme .css files

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: var(--font-family-sans-serif);
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
