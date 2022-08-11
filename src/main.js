b9d32d4e88a42266882efcab76f1b88a82098395
theWituch <thewituch@gmail.com>
2022-08-11 05:51:23 +0200

    Code cleanup and auto-formatting

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import "@/assets/main.css"
-import "@/assets/theme.css"
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "@/assets/main.css";
+import "@/assets/theme.css";
 
-document.title = "Portfolio"
+document.title = "Portfolio";
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app");
