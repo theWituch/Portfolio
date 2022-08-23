ee2d70ff47e7e6abcaf446662a2421aa56c453c3
theWituch <thewituch@gmail.com>
2022-08-23 16:56:27 +0200

    Install material-icons npm package and use instead of CDN

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/assets/theme.css";
 import "@/assets/main.css";
+import "@/assets/css/tweaks/material-icons.css";
 
 document.title = "Portfolio";
 
