0c344ee78f635454aad40b294776842d400ed2a9
theWituch <thewituch@gmail.com>
2022-08-29 12:17:21 +0200

    Add BistableButton component

---
+++ src/components/toggles/BistableButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <input type="checkbox" :id="id" :checked="modelValue" @change="$emit('update:modelValue', $event.target.checked)" />
+  <label :for="id" class="w-100 px-2">{{ option }}</label>
+</template>
+
+<script>
+export default {
+  props: ["modelValue", "id", "option"],
+  emits: ["update:modelValue"],
+};
+</script>
+
+<style scoped>
+input[type="checkbox"] {
+  height: 0;
+  width: 0;
+  visibility: hidden;
+}
+label {
+  cursor: pointer;
+  background: var(--th-whiteblue);
+}
+
+input:checked + label {
+  background: var(--bs-yellow);
+}
+</style>
