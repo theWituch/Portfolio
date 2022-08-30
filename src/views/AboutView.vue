3fb9cacacafefaaea1a4bec908f8110b06f8d34e
theWituch <thewituch@gmail.com>
2022-08-30 14:30:51 +0200

    Formatted source code

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -132,7 +132,12 @@
               rel="noreferrer"
               class="text-reset uil uil-github"
             ></a>
-            <a href="https://discordapp.com/users/226816245125742599" target="blank" rel="noreferrer" class="text-reset uil uil-discord"></a>
+            <a
+              href="https://discordapp.com/users/226816245125742599"
+              target="blank"
+              rel="noreferrer"
+              class="text-reset uil uil-discord"
+            ></a>
             <a href="#youtube" target="blank" rel="noreferrer" class="text-reset uil uil-youtube"></a>
           </div>
         </div>
