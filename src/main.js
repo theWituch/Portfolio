b3e4e69ed580abb321e297b9183893751a71058c
theWituch <thewituch@gmail.com>
2022-09-08 19:08:27 +0200

    Move CSS styles to @/styles directory and load them from SCSS entrypoint

--- src/main.js
+++ src/main.js
@@ -2,10 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "@/assets/theme.css";
-import "@/assets/main.css";
-import "@/assets/css/tweaks/material-icons.css";
+import "@/styles/main.scss";
 
 document.title = "Portfolio";
 
