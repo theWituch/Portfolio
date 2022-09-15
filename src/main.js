1ceed783015c5ca55e41924ee9f00fa277a9bf60
theWituch <thewituch@gmail.com>
2022-09-15 23:22:15 +0200

    Use VueSimpleBem in Vue app

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueSimpleBem from 'vue-simple-bem';
 
 import "@/styles/main.scss";
 
 document.title = "Portfolio";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App)
+app.use(router)
+app.use(VueSimpleBem)
+
+app.mount("#app");
