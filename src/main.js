43ea8f06fc25e5aa5beb7b2b3f83229856e1aaac
theWituch <thewituch@gmail.com>
2022-09-18 04:55:30 +0200

    Move main.scss import (global styles entrypoint) to the beginning of 'main.js' file
    This allow to keep in-component style to be loaded after and be higher in cascade

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
+// Important!
+// It is necessary to import global styles before importing the application so that in-component styles will be higher in the cascade
+import "@/styles/main.scss";
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import VueSimpleBem from 'vue-simple-bem';
-
-import "@/styles/main.scss";
+import bem from "vue-simple-bem";
 
 document.title = "Portfolio";
 
-const app = createApp(App)
-app.use(router)
-app.use(VueSimpleBem)
+const app = createApp(App);
+app.use(router);
+app.use(bem);
 
 app.mount("#app");
